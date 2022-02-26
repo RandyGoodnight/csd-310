f413007741e2d39dd269fa2a523c400d4dc29580
RandyGoodnight <99131954+RandyGoodnight@users.noreply.github.com>
2022-02-26 10:08:24 -0600

    This is my commit for module_9

--- Module_8/pysports_queries.py
+++ Module_8/pysports_queries.py
@@ -44,6 +44,18 @@ try:
 
     input("\n\n  Press any key to continue... ")
 
+except mysql.connector.Error as err:
+    """ handle errors """
+
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("  The supplied username or password are invalid")
+
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("  The specified database does not exist")
+
+    else:
+        print(err)
+
 finally:
     """ close the connection to MySQL """
     
